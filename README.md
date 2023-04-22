9930b6420954a0ff1036621f33f6ff8fd98d7764
Amanda Gray <floridabelle2021@protonmail.com>
2023-04-21 22:34:22 -0600

    tested my code many times haha, debugged it, and cleaned it up

--- README.md
+++ README.md
@@ -1,3 +1,61 @@
-# Professional README Generator 
+## Project Title
+  
+dsf
+
+![GitHub license](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)
+
+## Description
+
+dfds
 
 
+
+## Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+
+* [License](#license)
+
+
+* [Questions](#questions)
+
+## Installation 
+
+To install necessary dependencies, run the following command:
+
+```
+dsfds
+```
+
+## Usage 
+dfasd
+
+
+## License
+
+This project is licensed under the BSD 3-Clause license.
+
+
+## Contributing 
+fdsadf
+
+## Tests 
+To run tests, run the following command:
+
+```
+dfd
+```
+
+
+## Questions 
+
+If you have any questions about the repo, open an issue or contact me directly at dsfad.
+  <br>
+  You can find more of my work at [dsafs](https://github.com/dsafs/).
--- index.js
+++ index.js
@@ -1,83 +1,81 @@
 // These are the packages needed for this application
 const inquirer = require('inquirer');
-const fs = requirer('fs');
+const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
-//This is an array of questions for user input
+//This is an array of questions for user to input their answers
 const questions = [
     {
-        type: 'input',
-        message: 'what is your project title?',
-        name: 'title',
+      type: 'input',
+      message: 'What is your project title?',
+      name: 'title',
     },
     {
-        type: 'input',
-        message: 'Enter you project desription?',
-        name: 'description',
+      type: 'input',
+      message: 'Enter your project description',
+      name: 'description',
     },
     {
-        type: 'input',
-        message: 'Provide and example of code',
-        name: 'code',
+      type: 'input', 
+      message: 'Enter the command to install necessary dependencies',
+      name: 'installation',
     },
     {
-        type: 'input',
-        message: 'Provide information about contributers?',
-        name: 'contributing',
-    }, {
-        type: 'input',
-        message: 'provide information on running test'
-        name: 'test',
+      type: 'input', 
+      message: 'Provide an example of usage',
+      name: 'usage',
     },
     {
-        type: 'list',
-        message: ['apache 2.0', 'Boost 1.0', 'BSD 3-Clause', 'BSD 2-clause', 'IBM Public License Version 1.0', 'GNU FDL v1.3', 'GNU LGPL v3', 'GNU GPL v2'],
-        name: 'license',
+      type: 'input', 
+      message: 'Provide information about contributing',
+      name: 'contributing',
     },
     {
-        type: 'input',
-        message: 'what is your project title?',
-        name: 'title',
-    },
+      type: 'input', 
+      message: 'Provide information about running tests',
+      name: 'test',
+    }, 
     {
-        type: 'input',
-        message: 'what is your Linkedin address?',
-        name: 'linkedin',
-    },
+      type: 'list', 
+      choices: ['Apache 2.0','BSD 3-Clause', 'GNU GPL v3', 'GNU GPL v2', 'GNU AGPL v3', 'GNU LGPL v3', 'GNU FDL v1.3', 'IBM Public License Version 1.0'],
+      name: 'license',
+    }, 
     {
-        type: 'input',
-        message: 'What is your Github username',
-        name: 'github',
-    },
-];
-
+      type: 'input', 
+      message: 'What is your email address?',
+      name: 'email',
+    }, 
+    {
+      type: 'input', 
+      message: 'What is your github username?',
+      name: 'github',
+    }, 
+  ]; 
+  
 //I created function propmts for the user to enter data and then it will be added to the readme file
 inquirer
-    .promopt(questions)
-    .then((date) => {
-        let markdown = generateMarkdown({
-            title: `${data.title}`,
-            desription: `${data.description}`,
-            instalation: `${data.instalation}`,
-            code: `${data.code}`,
-            contributing: `${data.contributing}`,
-            test: `${data.test}`,
-            license: `${data.license}`,
-            linkedin: `${data.linkedin}`,
-            github: `${data.github}`,
-        })
+  .prompt(questions)
+  .then((data) => {
+    let markdown = generateMarkdown ({
+      title: `${data.title}`,
+      description: `${data.description}`,
+      installation: `${data.installation}`,
+      usage: `${data.usage}`,
+      contributing: `${data.contributing}`,
+      test: `${data.test}`,
+      license: `${data.license}`,
+      email: `${data.email}`,
+      github: `${data.github}`,
+    })
         //This is the generateMarkdown(data)
         fs.writeFile('README.md', markdown, (err) => {
             if (err) {
                 console.log(err);
             } else {
-                console.log('it worked');
+                console.log('Your just created a professional Readme! Well done!');
             }
         })
     });
-
-// //This is a function to write README file
-// function writeToFile(fileName, data) {}
 
 //This is the function to initialize app
 function init() {}
